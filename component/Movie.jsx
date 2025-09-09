7c3e5dfb333a88bcd61a21c75a8dc1592f5b39e8
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-09-09 11:03:01 +0530

    adding delete function

--- component/Movie.jsx
+++ component/Movie.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import useFetch from "../useFetch"
 const Movies = ()=>{
+   const [successMessage, setSuccessMessage] = useState("");
    const {data, loading, error} = useFetch("https://backend-movie-aljr.vercel.app/movies");
    console.log(data);
+
+   const handleDelete = async (movieId) =>{
+    try{
+        const response = await fetch(`https://backend-movie-aljr.vercel.app/movies/${movieId}`,
+
+            {method : "DELETE"},
+        )
+
+        if(!response.ok){
+            throw "Failed to delete movie."
+        }
+        const data = await response.json();
+        if(data){
+          setSuccessMessage("Movie Deleted successfully!");
+          window.location.reload();
+        }
+
+    }catch(error){
+        console.log(error);
+    }
+   }
+
    return(
     <div>
         <ul>
         {data?.map((movie)=>(
-            <li key={movie._id}>{movie.title}</li>
+            <li key={movie._id}>{movie.title}<button onClick={()=> handleDelete(movie._id)}>Delete</button></li>
         ))}
         </ul>
+        <p>{successMessage}</p>
     </div>
    )
 
